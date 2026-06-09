c38c30d3454921ecd799c8a49d5d21a4284d8552
meghana <ponagantimeghana02@gmail.com>
2026-06-09 11:25:10 +0530

    data_cleaning.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # day5-nlp-fundamentals
 Day 5-NLP Fundamentals
+
+Task 1: Data Cleaning
---
+++ text_cleaning.py
@@ -0,0 +1,38 @@
+import re
+import nltk
+
+# Download required resources
+for resource in ['punkt', 'punkt_tab']:
+    try:
+        nltk.data.find(f'tokenizers/{resource}')
+    except LookupError:
+        nltk.download(resource)
+
+from nltk.tokenize import word_tokenize
+
+text = """
+Hello Everyone!!!
+
+Welcome to AI/ML Training.
+
+Contact us at support@company.com
+
+Visit https://company.com
+
+Call 9876543210
+"""
+
+# Text cleaning
+text = text.lower()
+text = re.sub(r'https?://\S+', '', text)
+text = re.sub(r'\S+@\S+', '', text)
+text = re.sub(r'\b\d{10}\b', '', text)
+text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+tokens = word_tokenize(text)
+
+print("Tokens:")
+print(tokens)
+
+print("\nCleaned Text:")
+print(" ".join(tokens))
